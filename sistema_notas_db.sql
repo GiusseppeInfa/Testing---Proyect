20fae98d8cf9b79b368db8b000dcf51e808ab7a5
Bruker Nuñez <150071536+pierobruker@users.noreply.github.com>
2026-05-17 23:46:56 -0500

    Add files via upload
    adtualizacion de la db.sql

--- sistema_notas_db.sql
+++ sistema_notas_db.sql
@@ -1,34 +1,114 @@
-DROP DATABASE sistema_notas_db;
-CREATE DATABASE IF NOT EXISTS sistema_notas_db;
-USE sistema_notas_db;
+-- MySQL dump 10.13  Distrib 8.0.36, for Linux (x86_64)
+--
+-- Host: localhost    Database: sistema_notas_db
+-- ------------------------------------------------------
+-- Server version	8.0.45-0ubuntu0.24.04.1
 
-CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    usuario VARCHAR(50) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
-    rol ENUM('admin', 'profesor') NOT NULL,
-    activo BOOLEAN DEFAULT TRUE,
-    creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE estudiantes (
-    codigo VARCHAR(20) PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+--
+-- Table structure for table `estudiantes`
+--
 
-CREATE TABLE notas (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    estudiante_codigo VARCHAR(20) NOT NULL,
-    valor DECIMAL(5,2) NULL,
-    es_nsp BOOLEAN DEFAULT FALSE,
-    profesor_id INT NOT NULL,
-    creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    actualizado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+DROP TABLE IF EXISTS `estudiantes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `estudiantes` (
+  `codigo` varchar(20) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `creado_en` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-    FOREIGN KEY (estudiante_codigo) REFERENCES estudiantes(codigo)
-        ON DELETE CASCADE,
+--
+-- Dumping data for table `estudiantes`
+--
 
-    FOREIGN KEY (profesor_id) REFERENCES usuarios(id)
-        ON DELETE RESTRICT
-);
+LOCK TABLES `estudiantes` WRITE;
+/*!40000 ALTER TABLE `estudiantes` DISABLE KEYS */;
+INSERT INTO `estudiantes` VALUES ('1','Leonardo Leonasco','2026-05-13 16:20:54'),('2','Ivan Reanio','2026-05-13 16:21:14'),('3','Adrian Quispe','2026-05-13 20:31:33');
+/*!40000 ALTER TABLE `estudiantes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `notas`
+--
+
+DROP TABLE IF EXISTS `notas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `notas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `estudiante_codigo` varchar(20) NOT NULL,
+  `valor` decimal(5,2) DEFAULT NULL,
+  `es_nsp` tinyint(1) DEFAULT '0',
+  `profesor_id` int NOT NULL,
+  `creado_en` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `actualizado_en` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `estudiante_codigo` (`estudiante_codigo`),
+  KEY `profesor_id` (`profesor_id`),
+  CONSTRAINT `notas_ibfk_1` FOREIGN KEY (`estudiante_codigo`) REFERENCES `estudiantes` (`codigo`) ON DELETE CASCADE,
+  CONSTRAINT `notas_ibfk_2` FOREIGN KEY (`profesor_id`) REFERENCES `usuarios` (`id`) ON DELETE RESTRICT
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `notas`
+--
+
+LOCK TABLES `notas` WRITE;
+/*!40000 ALTER TABLE `notas` DISABLE KEYS */;
+INSERT INTO `notas` VALUES (1,'1',5.00,0,2,'2026-05-13 16:22:37','2026-05-14 19:24:24'),(2,'1',NULL,1,2,'2026-05-13 16:22:46','2026-05-13 16:22:46'),(3,'1',NULL,1,2,'2026-05-13 16:33:39','2026-05-13 16:33:39'),(4,'2',14.00,0,2,'2026-05-13 16:37:51','2026-05-13 16:37:51'),(5,'2',3.00,0,2,'2026-05-13 16:38:03','2026-05-13 16:38:03');
+/*!40000 ALTER TABLE `notas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(50) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `rol` enum('admin','profesor') NOT NULL,
+  `activo` tinyint(1) DEFAULT '1',
+  `creado_en` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `usuario` (`usuario`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'admin','scrypt:32768:8:1$NrkeYRKE8Zc8XfMO$b2974b4d84c67596db1b22b73a454d7c329f9a93578983f51c45a90cbfc71851c8c2fa3f1207eaf37b9e87d0ff2396dda5c694132d74cafa4c990074124b7c64','admin',1,'2026-05-13 04:24:14'),(2,'profe1','scrypt:32768:8:1$FsdblSJRkPmhK8oH$daeebb5115e02bfcb0e62b6ca7ea7f98fd6444470379e0024b368e75afb763515b0c8435733392f3436bc4f7343e2c5ba07bff646f0657c269fb4677c59f8a7e','profesor',1,'2026-05-13 04:24:14');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-05-14 23:39:31
